a547979e1cd9a8fc4740161b6c1f3d19b9f9c2e3
murli-9585 <murli.m9585@gmail.com>
2016-02-10 10:58:16 -0800

    Initial commit of Ordered(N) btree. Printing Btree has issues which need fix.

---
+++ btree/btree.h
@@ -0,0 +1,20 @@
+/* Binary Tree (Order N) implementation. */
+
+#ifndef BTREE_H
+#define BTREE_H
+#include <stddef.h>
+#include "btree_priv.h"
+
+// TODO: Change public names with Btree prefix.
+extern int btree_init(btree_node **tree, size_t order, size_t key_size,
+		int (*comparer)(const void *, const void *));
+extern int btree_search(btree_node *tree, const void *key);
+extern int btree_minimum(btree_node *tree, void *value);
+extern int btree_maximum(btree_node *tree, void *value);
+extern int btree_insert(btree_node **tree, void *value);
+extern int btree_delete(btree_node *tree, void *value);
+extern void btree_print(btree_node *tree);
+extern int btree_height(btree_node *tree);
+
+#endif  /* BTREE_H */
+
---
+++ btree/btree_test.c
@@ -0,0 +1,56 @@
+#include "btree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+btree_node *
+test_insert(int val, int order) {
+		int i;
+		btree_node *btree = NULL;
+		btree_init(&btree, order, sizeof(int), NULL);
+		for (i=0;i<val;i++)
+				btree_insert(&btree, &i);
+		printf("Inserting Items in btree, Success!\n");
+		return btree;
+}
+
+void
+print_tree(btree_node *btree, int everything) {
+		btree_print(btree);
+
+		if (everything) {
+				int min, max;
+				int search_item = 25;
+				btree_minimum(btree, &min);
+				btree_maximum(btree, &max);
+				printf("Minimum: %d\n", min);
+				printf("Maximum: %d\n", max);
+				printf("Height: %d\n", btree_height(btree));
+				printf("Search 25, result: %d", btree_search(btree, &search_item));
+		}
+}
+
+void
+help() {
+		printf("Inserts btree items from 0..n in btree of order N.\n"\
+				" ./btree_test <num_items_to_insert> <order> \n\n");
+		exit(0);
+}
+
+int
+main(int argc, char** argv) {
+		if (argc == 2 || argc == 3) {
+				int  num_inserts = 0;
+				int order = 0;
+				char *end;
+				num_inserts = strtol(argv[1], &end, 10);
+				if (argc ==3)
+						order = strtol(argv[2], &end, 10);
+				btree_node *tree = test_insert(num_inserts, order > 0 ? order : 3);
+				print_tree(tree, 1);
+		}
+		else
+				help();
+		return 0;
+}
+
